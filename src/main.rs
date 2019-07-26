20eb6d5fc9c829b1af57d8fa41210c47d3ea2266
Thomas Sullivan <thomas.l.sullivan@vumc.org>
2019-07-26 12:41:04 -0500

    comments in code are updated.

--- src/main.rs
+++ src/main.rs
@@ -14,13 +14,19 @@ use std::io::prelude::*;
 
 fn main() -> io::Result<()> {
     let stdin = io::stdin();
+
+    // TODO: Configure this via control file and in a safe way
     let date_re = Regex::new(r"^\d{4}-\d{2}-\d{2}.*").unwrap();
 
+    // For each line from stdin, lets do stuff
     for line in stdin.lock().lines() {
         let this_line = line.unwrap();
+
+        // If the line begins with a timestamp, just print it back to the output
         if date_re.is_match(this_line.as_str()) {
             println!("{}", this_line);
         } else {
+            // otherwise, we can add our timestamp.
             println!(
                 "{} {}",
                 Local::now().format("%Y-%m-%d %H:%M:%S").to_string(),
